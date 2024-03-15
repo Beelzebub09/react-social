dea0c597d20b31186d6a91da5710f8c827fd9730
Shaunak <shaunak9422@gmail.com>
2024-03-15 15:33:11 +0530

    new

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -35,6 +35,11 @@ export const addRemoveFriend = async (req, res) => {
         const user = await User.findById(id);
         const friend = await User.findById(friendId);
 
+        if (user.friends.includes(friendId)) {
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friend.friends.filter((id) => id !== friendId);
+        }
+
     } catch (err) {
         res.status(404).json({ message: err.message });
     }
