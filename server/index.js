a9c5c3d73aa5a7280dfb3b8a2c006c45d9940cef
Shaunak <shaunak9422@gmail.com>
2024-03-15 12:26:15 +0530

    new

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +36,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.post("/auth/register", upload.single("picture"), register);
+
+app.post("/auth", authRoutes);
 
 const PORT = process.env.PORT || 6001;
 mongoose
