2d7992a36f9b839379bdd6bee10e69da4acb7439
Shaunak <shaunak9422@gmail.com>
2024-03-15 11:32:21 +0530

    new

---
+++ server/controllers/auth.js
@@ -0,0 +1,20 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+    } catch (err) {
+
+    }
+}
