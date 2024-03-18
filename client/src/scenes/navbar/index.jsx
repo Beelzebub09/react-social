f144ccf12dcc8b66722900139a7a90533f252965
Shaunak <shaunak9422@gmail.com>
2024-03-18 15:14:02 +0530

    new

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = "{`${user.firstName} ${user.lastName}`}";
+  const fullName = "WELCOME GUEST ----";
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
