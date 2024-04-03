5f9e312cc7980100db36d1a2414dcb9470760913
Shaunak <shaunak9422@gmail.com>
2024-04-03 14:46:55 +0530

    new

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,27 +8,27 @@ import {
 import { verifyToken } from "../middleware/auth.js";
 import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 
-export const router1 = express.Router();
-export const router2 = express.Router();
-export const router3 = express.Router();
+export const authRouter = express.Router();
+export const userRouter = express.Router();
+export const postRouter = express.Router();
 
 
-router1.post("/login", login);
-
-/* READ */
-router2.get("/:id", verifyToken, getUser);
-router2.get("/:id/friends", verifyToken, getUserFriends);
-
-/* UPDATE */
-router2.patch("/:id/:friendId", verifyToken, addRemoveFriend);
-
+authRouter.post("/login", login);
 
 /* READ */
-router3.get("/", verifyToken, getFeedPosts);
-router3.get("/:userId/posts", verifyToken, getUserPosts);
+userRouter.get("/:id", verifyToken, getUser);
+userRouter.get("/:id/friends", verifyToken, getUserFriends);
 
 /* UPDATE */
-router3.patch("/:id/like", verifyToken, likePost);
+userRouter.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+
+/* READ */
+postRouter.get("/", verifyToken, getFeedPosts);
+postRouter.get("/:userId/posts", verifyToken, getUserPosts);
+
+/* UPDATE */
+postRouter.patch("/:id/like", verifyToken, likePost);
 
 
 
